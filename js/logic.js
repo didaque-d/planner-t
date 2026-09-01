278d318740f2fb033a0d76d8b2a51319ab354b64
didaque-d <diego.dads52@gmail.com>
2026-09-01 20:24:11 -0300

    Adiciona estrutura inicial do projeto com HTML, CSS e JavaScript, incluindo layout e funcionalidades básicas

---
+++ js/logic.js
@@ -0,0 +1,18 @@
+const disciplinas = [
+    {id: 0, aulas: [
+        "Windows 1: Introducão",
+        "Windows 2: Arquivos e Pastas"
+    ]},
+    {id: 1, aulas: [
+        "1: Introdução ao Word",
+        "2: Imagens e Formas"
+    ]}
+];
+
+function disciplinaSelecionada(){
+    const valorSelecionado = document.getElementById("disciplinas").value;
+    const {aulas} = disciplinas[valorSelecionado];
+    document.getElementById("infoAula").innerHTML = aulas.join("<br>");
+}
+disciplinaSelecionada();
+
